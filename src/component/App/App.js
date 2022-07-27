105a8d74039d754989f1c0881201cd2b13ae11a6
ghavasi <g.havasi@outlook.com>
2022-07-27 16:13:41 +0100

    Initial commit

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>I am a beautiful Final Project App</h1>
+      <h1>I am a beautiful Final Project App!</h1>
     </div>
   );
 }
