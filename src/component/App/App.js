122daad3d49c50f4a22f9a28267d1d2b5a8dacee
eda <edaburns@gmail.com>
2022-07-28 11:45:04 +0100

    Css started

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -1,9 +1,10 @@
+import SearchResult from "../SearchResult/SearchResult";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>I am a beautiful Final Project App!</h1>
+      <SearchResult/>
     </div>
   );
 }
