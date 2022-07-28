19119ac00299aeee90a705131eff763676a6efe2
nafisoaden97 <nafisoaden97@gmail.com>
2022-07-28 11:45:47 +0100

    nav bar started

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -1,8 +1,17 @@
 import "./App.css";
-
+import Navbar from "../Navbar/Navbar.js";
+import Landingpage from "../../images/Landingpage.jpg";
 function App() {
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${Landingpage})`,
+        height: "100vh",
+        backgroundSize: "100% 100%",
+      }}
+    >
+      <Navbar />
       <h1>I am a beautiful Final Project App!</h1>
     </div>
   );
