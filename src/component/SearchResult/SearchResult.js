340399179fb7128af3eb2ae0362e1073fe7ce605
eda <edaburns@gmail.com>
2022-07-28 15:18:56 +0100

    top and bottom images trials

--- src/component/SearchResult/SearchResult.js
+++ src/component/SearchResult/SearchResult.js
@@ -1,12 +1,34 @@
+import { useEffect, useState } from "react";
 import "./SearchResult.css";
 
 const SearchResult = () => {
+  const [imageUrl, setImageUrl] = useState("");
+  const location = "London";
+  const ApiKey = "-csYdQoUNZY1UOcp4zEnsOVjNZyBXTmDfjJcair8Q6o";
+  const requestUrl =
+  `https://api.unsplash.com/search/photos?query=${location}&orientation=landscape&client_id=${ApiKey}`;
+
+  useEffect(() => {
+    getLocationImage();
+  }, []);
+
+  async function getLocationImage() {
+    let randomNumber = Math.floor(Math.random() * 5);
+    const response = await fetch(requestUrl);
+    const data = await response.json();
+    let oneImage = data.results[randomNumber];
+    setImageUrl(oneImage.urls.full);
+  }
+
   return (
     <div className="results-page">
-      <div className="top-picture"></div>
+      <div className="top-picture">
+        <img src={imageUrl} alt="Location" className="location-image"></img>
+        {/* <p className="test">LONDON</p> */}
+      </div>
       <div className="bottom-results">
-        <div className="map"></div>
         <div className="text"></div>
+        <div className="map"></div>
       </div>
     </div>
   );
