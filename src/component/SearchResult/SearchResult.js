049e36052403bc6d31510090ef7c78d3dc261cbd
pattisoj <joshpattison23@icloud.com>
2022-07-28 11:57:07 +0100

    CSS for backround div

--- src/component/SearchResult/SearchResult.js
+++ src/component/SearchResult/SearchResult.js
@@ -1,8 +1,10 @@
+import "./SearchResult.css";
+
 const SearchResult = () => {
   return (
-    <div className = "results-page" >
+    <div className="results-page">
       <div className="top-picture"></div>
-      <div className= "bottom-results ">
+      <div className="bottom-results">
         <div className="map"></div>
         <div className="text"></div>
       </div>
