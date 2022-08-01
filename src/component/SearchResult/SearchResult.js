90c55be6765eb55e8722289c1cd6c386029dc421
ghavasi <g.havasi@outlook.com>
2022-08-01 14:55:23 +0100

    Gov API was added

--- src/component/SearchResult/SearchResult.js
+++ src/component/SearchResult/SearchResult.js
@@ -4,13 +4,20 @@ import MapContainer from "../Map/Map.js";
 
 const SearchResult = ({ searchTerm }) => {
   const [imageUrl, setImageUrl] = useState("");
-  // const searchTerm = "London";
+  const [govAPI, setGovAPI] = useState("");
+  const searchTermGov = searchTerm.toLowerCase();
   const ApiKey = process.env.REACT_APP_UNSPLASH;
   const requestUrl = `https://api.unsplash.com/search/photos?query=${searchTerm}&orientation=landscape&client_id=${ApiKey}`;
 
   useEffect(() => {
     getLocationImage();
   }, []);
+
+  let ConvertStringToHTML = function (str) {
+    let parser = new DOMParser();
+    let doc = parser.parseFromString(str, "text/html");
+    return doc.body;
+  };
 
   async function getLocationImage() {
     let randomNumber = Math.floor(Math.random() * 5);
@@ -20,11 +27,14 @@ const SearchResult = ({ searchTerm }) => {
     setImageUrl(oneImage.urls.full);
 
     const govResponse = await fetch(
-      `https://www.gov.uk/api/content/foreign-travel-advice/${searchTerm}`
+      `https://www.gov.uk/api/content/foreign-travel-advice/${searchTermGov}`
     );
     const govData = await govResponse.json();
-    console.log(govData);
+    console.log(govData.details.summary);
+    setGovAPI(govData.details.summary);
   }
+
+  let govText = ConvertStringToHTML(govAPI);
 
   return (
     <div className="results-page">
@@ -38,7 +48,10 @@ const SearchResult = ({ searchTerm }) => {
         <p className="country-name">{searchTerm.toUpperCase()}</p>
       </div>
       <div className="bottom-results">
-        <div className="text"></div>
+        <div
+          className="text"
+          dangerouslySetInnerHTML={{ __html: govAPI }}
+        ></div>
         <div className="map">
           <MapContainer />
         </div>
