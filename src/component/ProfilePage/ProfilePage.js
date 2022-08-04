5a36e5342c0d9752fa1626564c9ccb0414797d1d
nafisoaden97 <nafisoaden97@gmail.com>
2022-08-04 10:39:00 +0100

    get request working

--- src/component/ProfilePage/ProfilePage.js
+++ src/component/ProfilePage/ProfilePage.js
@@ -1,6 +1,7 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 import "./ProfilePage.css";
+import Axios from "axios";
 
 const Profile = ({ user, isAuthenticated, isLoading }) => {
   if (isLoading) {
@@ -8,12 +9,22 @@ const Profile = ({ user, isAuthenticated, isLoading }) => {
   }
   console.log(user);
 
+  async function getLocation() {
+    console.log("Inside Axios");
+    Axios.get(
+      "https://pacific-journey-78384.herokuapp.com/https://8a50g75era.execute-api.eu-west-2.amazonaws.com/prod/location"
+    ).then((response) => {
+      console.log(response.data.payload);
+      console.log(response.data);
+    });
+  }
   return (
     isAuthenticated && (
       <div>
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
+        <button onClick={getLocation}>press me</button>
       </div>
     )
   );
