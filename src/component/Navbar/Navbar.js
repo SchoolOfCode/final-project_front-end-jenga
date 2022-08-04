451a4cc33beabbbcdbf9b1e2e8ab7fb91780dc9f
pattisoj <joshpattison23@icloud.com>
2022-08-04 12:39:31 +0100

    More work on Navbar CSS - responsiveness

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -8,33 +8,34 @@ const Navbar = ({ user, isAuthenticated, isLoading }) => {
   return (
     <nav className="nav-container" aria-label="navbar">
       <div className="nav-appname">
-        <a className="nav-link" href="/">
-          <img className="balloon" src={svg} alt="SVG" />
-        </a>
-      </div>
-      <div className="title"> FINDERS KEEPERS</div>
-      <ul className="home-buttons">
-        <li className="nav-point">
-          <a href="/">
-            <button className="nav-link home ">HOME</button>
+        <div className="balloon-container">
+          <a className="nav-link" href="/">
+            <img className="balloon" src={svg} alt="SVG" />
           </a>
-        </li>
-        <li className="nav-point">
-          {!isAuthenticated && <LoginButton />}
+        </div>
+      </div>
+      <h1 className="title"> FINDERS KEEPERS</h1>
+      <div className="nav-buttons-container">
+        <a href="/">
+          <button className="home-button">Home</button>
+        </a>
 
-          {isAuthenticated && <LogoutButton />}
+        {!isAuthenticated && <LoginButton />}
 
-          {isAuthenticated && (
+        {isAuthenticated && <LogoutButton />}
+
+        {isAuthenticated && (
+          <div className="profile-image-container">
             <Link to="/Profile">
               <img
-                className="profileImage"
+                className="profile-image"
                 src={user.picture}
                 alt={user.name}
               />
             </Link>
-          )}
-        </li>
-      </ul>
+          </div>
+        )}
+      </div>
     </nav>
   );
 };
